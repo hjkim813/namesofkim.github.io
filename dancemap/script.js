435dd6584b227e162b612feda491b66b43dbd989
namesofkim <30866302+imhjk@users.noreply.github.com>
2020-08-31 16:08:06 +0900

    gradshowupdate

--- dancemap/script.js
+++ dancemap/script.js
@@ -1,7 +1,4 @@
 
-// const local_data = data;
-// var data = require('./datacomma.json'); //(with path)
-
 const progressBar = document.getElementById('progress');
 const progressBarContainer = document.getElementsByClassName('slider')[0];
 const desiredTimeSpan = document.getElementById('desired-time');
@@ -20,144 +17,148 @@ function visualiseOutput(id) {
   numDancingBodies.innerHTML = entry['Dancing Bodies'];
   totalBodies.innerHTML = entry['Total Bodies'];
   firstcol.innerHTML = entry['Viewcount'];
-  secondcol.innerHTML = `<p> ${entry['Artist']} – ${entry['Song Title']} &ensp; ${entry['Date']} &ensp; ${entry['Company']} </p>`;
+  secondcol.innerHTML = `<p> ${entry['Artist']} – ${entry['Song Title']} &thinsp; ${entry['Date']} &thinsp; ${entry['Company']} &thinsp;&thinsp;</p>`;
 }
 
 function changeContent(time) {
-  if (time > 0 && time < 33) {
+  if (time > 0 && time < 32) {
     visualiseOutput(64);
-  } else if (time > 33 && time < 59) {
+  } else if (time > 33 && time < 58) {
     visualiseOutput(63);
-  } else if (time > 59 && time < 88) {
+  } else if (time > 59 && time < 89) {
     visualiseOutput(62);
-  } else if (time > 88 && time < 116) {
+  } else if (time > 90 && time < 115) {
     visualiseOutput(61);
-  } else if (time > 116 && time < 146) {
+  } else if (time > 116 && time < 145) {
     visualiseOutput(60);
-  } else if (time > 146 && time < 166) {
+  } else if (time > 146 && time < 165) {
     visualiseOutput(59);
-  } else if (time > 166 && time < 202) {
+  } else if (time > 166 && time < 201) {
     visualiseOutput(58);
-  } else if (time > 202 && time < 221) {
+  } else if (time > 202 && time < 220) {
     visualiseOutput(57);
-  } else if (time > 221 && time < 242) {
+  } else if (time > 221 && time < 241) {
     visualiseOutput(56);
-  } else if (time > 242 && time < 263) {
+  } else if (time > 242 && time < 262) {
     visualiseOutput(55);
-  } else if (time > 263 && time < 285) {
+  } else if (time > 263 && time < 284) {
     visualiseOutput(54);
-  } else if (time > 285 && time < 310) {
+  } else if (time > 285 && time < 311) {
     visualiseOutput(53);
-  } else if (time > 310 && time < 341) {
+  } else if (time > 312 && time < 342) {
     visualiseOutput(52);
-  } else if (time > 341 && time < 365) {
+  } else if (time > 343 && time < 364) {
     visualiseOutput(51);
-  } else if (time > 365 && time < 394) {
+  } else if (time > 365 && time < 393) {
     visualiseOutput(50);
-  } else if (time > 394 && time < 419) {
+  } else if (time > 394 && time < 418) {
     visualiseOutput(49);
-  } else if (time > 419 && time < 435) {
+  } else if (time > 418 && time < 434) {
     visualiseOutput(48);
-  } else if (time > 435 && time < 465) {
+  } else if (time > 435 && time < 464) {
     visualiseOutput(47);
-  } else if (time > 465 && time < 491) {
+  } else if (time > 465 && time < 490) {
     visualiseOutput(46);
-  } else if (time > 491 && time < 516) {
+  } else if (time > 491 && time < 515) {
     visualiseOutput(45);
-  } else if (time > 516 && time < 554) {
+  } else if (time > 516 && time < 553) {
     visualiseOutput(44);
-  } else if (time > 554 && time < 574) {
+  } else if (time > 554 && time < 573) {
     visualiseOutput(43);
-  } else if (time > 574 && time < 592) {
+  } else if (time > 574 && time < 591) {
     visualiseOutput(42);
-  } else if (time > 592 && time < 610) {
+  } else if (time > 592 && time < 619) {
     visualiseOutput(41);
-  } else if (time > 610 && time < 633) {
+  } else if (time > 620 && time < 632) {
     visualiseOutput(40);
-  } else if (time > 633 && time < 652) {
+  } else if (time > 633 && time < 651) {
     visualiseOutput(39);
-  } else if (time > 652 && time < 692) {
+  } else if (time > 652 && time < 691) {
     visualiseOutput(38);
-  } else if (time > 692 && time < 730) {
+  } else if (time > 692 && time < 739) {
     visualiseOutput(37);
-  } else if (time > 730 && time < 756) {
+  } else if (time > 740 && time < 755) {
     visualiseOutput(36);
-  } else if (time > 756 && time < 787) {
+  } else if (time > 756 && time < 786) {
     visualiseOutput(35);
-  } else if (time > 787 && time < 819) {
+  } else if (time > 787 && time < 818) {
     visualiseOutput(34);
-  } else if (time > 819 && time < 844) {
+  } else if (time > 819 && time < 843) {
     visualiseOutput(33);
-  } else if (time > 844 && time < 865) {
+  } else if (time > 844 && time < 864) {
     visualiseOutput(32);
-  } else if (time > 865 && time < 882) {
+  } else if (time > 865 && time < 881) {
     visualiseOutput(31);
-  } else if (time > 882 && time < 902) {
+  } else if (time > 882 && time < 901) {
     visualiseOutput(30);
-  } else if (time > 902 && time < 935) {
+  } else if (time > 902 && time < 934) {
     visualiseOutput(29);
-  } else if (time > 935 && time < 969) {
+  } else if (time > 935 && time < 968) {
     visualiseOutput(28);
-  } else if (time > 969 && time < 996) {
+  } else if (time > 969 && time < 995) {
     visualiseOutput(27);
-  } else if (time > 996 && time < 1025) {
+  } else if (time > 996 && time < 1024) {
     visualiseOutput(26);
-  } else if (time > 1025 && time < 1043) {
+  } else if (time > 1025 && time < 1042) {
     visualiseOutput(25);
-  } else if (time > 1043 && time < 1060) {
+  } else if (time > 1043 && time < 1059) {
     visualiseOutput(24);
-  }else if (time > 1060 && time < 1092) {
+  }else if (time > 1060 && time < 1091) {
     visualiseOutput(23);
-  } else if (time > 1092 && time < 1112) {
+  } else if (time > 1092 && time < 1111) {
     visualiseOutput(22);
-  } else if (time > 1112 && time < 1140) {
+  } else if (time > 1112 && time < 1139) {
     visualiseOutput(21);
-  } else if (time > 1140 && time < 1173) {
+  } else if (time > 1140 && time < 1172) {
     visualiseOutput(20);
-  } else if (time > 1173 && time < 1202) {
+  } else if (time > 1173 && time < 1201) {
     visualiseOutput(19);
-  } else if (time > 1202 && time < 1235) {
+  } else if (time > 1202 && time < 1234) {
     visualiseOutput(18);
-  } else if (time > 1235 && time < 1281) {
+  } else if (time > 1235 && time < 1280) {
     visualiseOutput(17);
-  } else if (time > 1281 && time < 1313) {
+  } else if (time > 1281 && time < 1312) {
     visualiseOutput(16);
-  } else if (time > 1313 && time < 1348) {
+  } else if (time > 1313 && time < 1347) {
     visualiseOutput(15);
-  } else if (time > 1348 && time < 1377) {
+  } else if (time > 1348 && time < 1376) {
     visualiseOutput(14);
-  } else if (time > 1377 && time < 1406) {
+  } else if (time > 1377 && time < 1405) {
     visualiseOutput(13);
-  } else if (time > 1406 && time < 1438) {
+  } else if (time > 1406 && time < 1437) {
     visualiseOutput(12);
-  } else if (time > 1438 && time < 1469) {
+  } else if (time > 1438 && time < 1468) {
     visualiseOutput(11);
-  } else if (time > 1469 && time < 1506) {
+  } else if (time > 1469 && time < 1505) {
     visualiseOutput(10);
-  } else if (time > 1506 && time < 1537) {
+  } else if (time > 1506 && time < 1536) {
     visualiseOutput(9);
-  } else if (time > 1537 && time < 1560) {
+  } else if (time > 1537 && time < 1559) {
     visualiseOutput(8);
-  }else if (time > 1560 && time < 1587) {
+  }else if (time > 1560 && time < 1586) {
     visualiseOutput(7);
-  } else if (time > 1587 && time < 1615) {
+  } else if (time > 1587 && time < 1614) {
     visualiseOutput(6);
-  } else if (time > 1615 && time < 1638) {
+  } else if (time > 1615 && time < 1637) {
     visualiseOutput(5);
-  } else if (time > 1638 && time < 1657) {
+  } else if (time > 1638 && time < 1656) {
     visualiseOutput(4);
-  } else if (time > 1657 && time < 1673) {
+  } else if (time > 1567 && time < 1672) {
     visualiseOutput(3);
-  } else if (time > 1673 && time < 1693) {
+  } else if (time > 1673 && time < 1692) {
     visualiseOutput(2);
-  } else if (time > 1693 && time < 1718) {
+  } else if (time > 1693 && time < 1717) {
     visualiseOutput(1);
   } 
 }
 
 function updatePlayer(currentTime, totalTime) {
+
+  console.log(currentTime);
+  
   progress = (currentTime / totalTime) * 100;
-  progressBar.style.width = `${progress}%`;
+  progressApx = progress.toFixed(1);
+  progressBar.style.width = `${progressApx}%`;
 
   let currentTimeFormatted = new Date(currentTime * 1000)
     .toISOString()
@@ -168,20 +169,25 @@ function updatePlayer(currentTime, totalTime) {
     .substr(11, 8);
 
   currentTimeSpan.innerHTML = `${currentTimeFormatted} / ${totalTimeFormatted}`;
-}
+  // if (currentTimeSpan.innerHTML == `${currentTimeFormatted} / ${totalTimeFormatted}`){
+  //   clearInterval();
+  //   }
+};
+
 
 function clickOnPlayer(player, totalTime) {
   let progressBarWidth = progressBarContainer.clientWidth;
 
   progressBarContainer.addEventListener('mousemove', (e) => {
     let desiredTimeSpanPosition = e.offsetX - desiredTimeSpan.clientWidth / 2;
+    let desiredTimeSpanPositionApx = desiredTimeSpanPosition.toFixed(1);
     let desiredTime = (e.offsetX / progressBarWidth) * totalTime;
     let desiredTimeFormatted = new Date(desiredTime * 1000)
       .toISOString()
       .substr(11, 8);
 
     desiredTimeSpan.classList.add('active');
-    desiredTimeSpan.style.left = `${desiredTimeSpanPosition}px`;
+    desiredTimeSpan.style.left = `${desiredTimeSpanPositionApx}px`;
     desiredTimeSpan.innerHTML = desiredTimeFormatted;
   });
 
@@ -209,18 +215,29 @@ $(document).ready(function(){
   $('#howto').hide();
   $('#pleaseClick').hide();
 
-  setTimeout(function(){
-      $('#pleaseWait').fadeIn(1000);
-  },1000);
-  setTimeout(function(){
-    $('#howto').fadeIn(1000);
-},3000);
+//   setTimeout(function(){
+//       $('#pleaseWait').fadeIn(1000);
+//   },1000);
+//   setTimeout(function(){
+//     $('#howto').fadeIn(1000);
+// },3000);
 
-  setTimeout(function(){
-    $('#pleaseClick').fadeIn(1000);
-},10000);
+//   setTimeout(function(){
+//     $('#pleaseClick').fadeIn(1000);
+// },10000);
+// });
+
+setTimeout(function(){
+  $('#pleaseWait').fadeIn(1000);
+},100);
+setTimeout(function(){
+$('#howto').fadeIn(1000);
+},300);
+
+setTimeout(function(){
+$('#pleaseClick').fadeIn(1000);
+},1000);
 });
-
 
 document.getElementById("pleaseClick").addEventListener("click", function(){
   $(".instructions").css({"display":"none"})
@@ -248,7 +265,6 @@ function onYouTubeIframeAPIReady() {
       fs: 0,
       modestbranding: 1,
       frameborder:0,
-      vq:'highres'
     },
     events: {
       onReady: onPlayerReady,
@@ -259,32 +275,32 @@ function onYouTubeIframeAPIReady() {
 
 // 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
-  event.target.setPlaybackQuality('highres');
   event.target.playVideo();
-
 }
 
 // 5. The API calls this function when the player's state changes.
 //    The function indicates that when playing a video (state=1),
 function onPlayerStateChange(event) {
+
   if (event.data == YT.PlayerState.PLAYING) {
-    event.target.setPlaybackQuality('highres');
+
     var get = $("#current-time");
     var txt = get.html();
     if (txt == ""){
       player.pauseVideo();
       console.log("video paused")
     }
-    
-    // Set the onclick event to the button for pause the YouTube video.
-    setInterval(() => {
-      let currentTime = player.getCurrentTime();
-      let totalTime = player.getDuration();
-      changeContent(currentTime);
-      updatePlayer(currentTime, totalTime);
-      clickOnPlayer(player, totalTime);
-    }, 1000);
 
+
+      setInterval(() => {
+        let currentTimeMax = player.getCurrentTime();
+        let currentTime = parseInt(currentTimeMax);
+        let totalTime = player.getDuration();
+        changeContent(currentTime);
+        updatePlayer(currentTime, totalTime);
+        clickOnPlayer(player, totalTime);
+      }, 1000);
+  
     document.getElementById('box').onclick = function() {
       player.pauseVideo();
       console.log("video paused")
@@ -301,6 +317,7 @@ function onPlayerStateChange(event) {
       document.getElementById('box').onclick = function() {
         player.playVideo();
         console.log("video plays")
+
     };           
       document.body.onkeyup = function(e){
         if(e.keyCode == 32){
@@ -310,12 +327,13 @@ function onPlayerStateChange(event) {
   }
 }
 
+
 window.onkeydown = function(e) { 
   return !(e.keyCode == 32 && e.target == document.body);
 }; 
 
 function stopVideo() {
-  console.log('asdds');
+  console.log('initial video pause');
 }
 
 document.getElementById("plus").addEventListener("click", function(){
@@ -324,27 +342,26 @@ document.getElementById("plus").addEventListener("click", function(){
   var container = document.getElementsByClassName('container')[0];
   var matrix = $('.container').css('transform');
 
-  // console.log(matrix) 
 
   if (matrix==`matrix(0.5, 0, 0, 0.5, 0, 0)`){
     container.style.setProperty('transform', 'scale(0.75)');
-    div.style.top = "-40%";
-    div.style.left = "-75%";
+    div.style.top = "-50%";
+    div.style.left = "-100%";
     }
   else if (matrix==`matrix(0.75, 0, 0, 0.75, 0, 0)`){
     container.style.setProperty('transform', 'scale(1)');
     div.style.top = "-25%";
-    div.style.left = "-75%";
+    div.style.left = "-95%";
     }
   else if (matrix==`matrix(1, 0, 0, 1, 0, 0)`){
     container.style.setProperty('transform', 'scale(1.5)');
     div.style.top = "-10%";
-    div.style.left = "-30%";
+    div.style.left = "-80%";
     }
   else if (matrix==`matrix(1.5, 0, 0, 1.5, 0, 0)`) {
     container.style.setProperty('transform', 'scale(2)');
     div.style.top = "0";
-    div.style.left = "-30%";
+    div.style.left = "-100%";
   }
 });
 
@@ -355,27 +372,48 @@ document.getElementById("minus").addEventListener("click", function(){
 
   if (matrix==`matrix(0.75, 0, 0, 0.75, 0, 0)`){
     container.style.setProperty('transform', 'scale(0.5)');
-    div.style.top = "-55%";
-    div.style.left = "-100%";
+    div.style.top = "-65%";
+    div.style.left = "-120%";
   }
   else if (matrix==`matrix(1, 0, 0, 1, 0, 0)`){
     container.style.setProperty('transform', 'scale(0.75)');
-    div.style.top = "-40%";
-    div.style.left = "-75%";
+    div.style.top = "-50%";
+    div.style.left = "-100%";
   }
   else if (matrix==`matrix(1.5, 0, 0, 1.5, 0, 0)`){
     container.style.setProperty('transform', 'scale(1)');
     div.style.top = "-25%";
-    div.style.left = "-75%";
+    div.style.left = "-90%";
   }
   else if (matrix==`matrix(2, 0, 0, 2, 0, 0)`) {
     container.style.setProperty('transform', 'scale(1.5)');
     div.style.top = "-10%";
-    div.style.left = "-30%";
+    div.style.left = "-80%";
   }
   else if (matrix==`matrix(2.5, 0, 0, 2.5, 0, 0)`) {
     container.style.setProperty('transform', 'scale(2)');
     div.style.top = "0";
-    div.style.left = "-30%";
+    div.style.left = "-100%";
   }
 });
+
+var idleTime = 0;
+$(document).ready(function () {
+    //Increment the idle time counter every minute.
+    var idleInterval = setInterval(timerIncrement, 60000); // 1 minute
+
+    //Zero the idle timer on mouse movement.
+    $(this).mousemove(function (e) {
+        idleTime = 0;
+    });
+    $(this).keypress(function (e) {
+        idleTime = 0;
+    });
+});
+
+function timerIncrement() {
+    idleTime = idleTime + 1;
+    if (idleTime > 5) { // 5 minutes
+        window.location.reload();
+    }
+}
