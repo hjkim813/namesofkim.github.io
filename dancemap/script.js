64d5bc1c176aa8b948b5ce646c49b0317b2981ce
namesofkim <30866302+imhjk@users.noreply.github.com>
2020-09-05 23:17:16 +0900

    autoplayfix

--- dancemap/script.js
+++ dancemap/script.js
@@ -265,6 +265,7 @@ function onYouTubeIframeAPIReady() {
 // 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
   event.target.playVideo();
+  console.log("video auto plays")
 }
 
 // 5. The API calls this function when the player's state changes.
