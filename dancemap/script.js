0096214e01bc392cbd4f0dc63e347df4cda0e683
namesofkim <30866302+imhjk@users.noreply.github.com>
2020-09-05 23:45:10 +0900

    txt=00

--- dancemap/script.js
+++ dancemap/script.js
@@ -275,7 +275,7 @@ function onPlayerStateChange(event) {
   if (event.data == YT.PlayerState.PLAYING) {
     var get = $("#current-time");
     var txt = get.html();
-    if (txt == ""){
+    if (txt == "00:00:00 / 00:28:37"){
       player.pauseVideo();
       console.log("video paused for load")
     }
